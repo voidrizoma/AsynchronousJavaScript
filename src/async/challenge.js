c76c9d769f29c78af3a44baae9d15378a0326450
voidrizoma <milhadasdepapel@gmail.com>
2021-08-18 22:58:58 -0500

    fetch api async functions

---
+++ src/async/challenge.js
@@ -0,0 +1,20 @@
+const fetchData = require('../utils/fetch')
+const API = "https://rickandmortyapi.com/api/character/"
+
+const anotherFunction = async (url) => {
+    try {
+        const data = await fetchData(url)
+        const character = await fetchData(`${API}${data.results[0].id}`)
+        const origin = await fetchData(character.origin.url)
+
+        console.log(data.info.count)
+        console.log(character.name)
+        console.log(origin.dimension)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+console.log('before')
+anotherFunction(API)
+console.log('After')
